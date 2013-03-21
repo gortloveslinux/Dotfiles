5e5620139f6fbdfd728d0d43c47cfab9b8bb0ed1
cme <cme@dev48.boi.keynetics.com>
2013-03-21 16:08:34 -0700

    Added rainbow-road to functions.sh

--- .bash_aliases
+++ .bash_aliases
@@ -1,6 +1,6 @@
 alias ls='ls --color=auto'
-alias ll='ls -alF' 
-alias la='ls -A' 
+alias ll='ls -alF'
+alias la='ls -A'
 alias sl='ls'
 alias ra='sudo /etc/init.d/apache2 restart'
 alias t='tree -L 2'
--- bin/functions.sh
+++ bin/functions.sh
@@ -1,3 +1,16 @@
 function cl {
   cd "$*" && ls
 }
+
+function rainbow-road {
+  i=$(( 1 ))
+  tail -f ${@} | while read line; do
+    trap 'tput sgr0 && exit 1' INT
+    echo -e $line | grep -q "^==>.*<==$"
+    if [ $? -eq 0 ]; then
+      tput setaf $(( $i % 7 + 1))
+      i=$(( $i + 1 ))
+    fi
+    echo -e $line
+  done
+}
